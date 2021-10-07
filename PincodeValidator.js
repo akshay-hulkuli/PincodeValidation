69c2c252f2b40c1a4f88749919b93069a16cad09
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-07 22:07:22 +0530

    ADDED: program to check if pincode has six digits

---
+++ PincodeValidator.js
@@ -0,0 +1,20 @@
+let pincodeArray = new Array();
+pincodeArray.push(400080);
+
+function pinCodeValidator(pincode){
+    let pinCodeRegex = RegExp("[0-9]{6}");
+    if(pinCodeRegex.test(pincode))
+        console.log("   passed");
+    else throw '    Pincode is not valid';
+}
+
+
+pincodeArray.forEach(pincode => {
+    console.log("pincode : "+ pincode)
+    try{
+        pinCodeValidator(pincode);
+    }
+    catch(e){
+        console.error(e);
+    }
+});
