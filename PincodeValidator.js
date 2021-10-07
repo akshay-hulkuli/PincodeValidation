4d62bc60760a0f1170206c8e5233a6be8069394c
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-07 22:10:03 +0530

    REFACTORED: regex to check no characters are taken in pincode

--- PincodeValidator.js
+++ PincodeValidator.js
@@ -1,8 +1,9 @@
 let pincodeArray = new Array();
-pincodeArray.push(400080);
+pincodeArray.push("400080");
+pincodeArray.push("A400080");
 
 function pinCodeValidator(pincode){
-    let pinCodeRegex = RegExp("[0-9]{6}");
+    let pinCodeRegex = RegExp("^[0-9]{6}");
     if(pinCodeRegex.test(pincode))
         console.log("   passed");
     else throw '    Pincode is not valid';
